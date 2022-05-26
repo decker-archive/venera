3c63920a3b087b46829faed05a18a38899b7389f
VincentRPS <vincentbusiness55@gmail.com>
2022-05-26 17:27:24 +0800

    Add react-router to app

--- venera/app/src/main.tsx
+++ venera/app/src/main.tsx
@@ -1,7 +1,12 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
+// <Route element={<NotFound />} />
 ReactDOM.createRoot(document.getElementById('zen')!).render(
-  <App />
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<App />} />
+    </Routes>
+  </BrowserRouter>
 )

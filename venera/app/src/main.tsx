499213c7026361f7d1833ee546e8cb0fdf9b7196
VincentRPS <vincentbusiness55@gmail.com>
2022-05-27 13:04:51 +0800

    Rename App to Index

--- venera/app/src/main.tsx
+++ venera/app/src/main.tsx
@@ -1,12 +1,12 @@
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import Index from './index'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 // <Route element={<NotFound />} />
-ReactDOM.createRoot(document.getElementById('zen')!).render(
+ReactDOM.createRoot(document.getElementById('app-mount')!).render(
   <BrowserRouter>
     <Routes>
-      <Route path='/' element={<App />} />
+      <Route path='/' element={<Index />} />
     </Routes>
   </BrowserRouter>
 )
